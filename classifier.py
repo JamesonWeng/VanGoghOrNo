f292c28bbc0c9e52a00c1d78aa144b741da87955
Jameson Weng <weng.jameson126@gmail.com>
2017-05-15 17:19:14 -0400

    storing & loading image dataset

--- classifier.py
+++ classifier.py
@@ -2,20 +2,32 @@
 
 import logging
 import matplotlib.pyplot as plt
-from os import listpath
+from os import listdir
 import os.path
 import pandas as pd
-import pickle
 from skimage import io
 
 def load_data_set(root_dir):
     data_set = []
 
+    # read the positive examples
+    positive_example_dir = root_dir + 'positive/'
+    for file_name in os.listdir(positive_example_dir):
+        file_path = positive_example_dir + file_name;
+        logging.debug('loading image from ' + file_path)
+        data_set.append((io.imread(file_path), True)) 
+
+    # read the negative examples
+    negative_example_dir = root_dir + 'negative/'
+    for file_name in os.listdir(negative_example_dir):
+        file_path = negative_example_dir + file_name;
+        logging.debug('loading image from ' + file_path)
+        data_set.append((io.imread(file_path), True)) 
+
+    return data_set
+
 
 logging.basicConfig(level=logging.DEBUG)
 
 logging.info('loading training set')
-training_set = load_data_set('data/training_set')
-
-plt.imshow(training_set[3][0])
-plt.show()
+training_set = load_data_set('data/training_set/')
