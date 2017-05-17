c18e3fba8e6f0660e5224757eb6bd7d55d081bc5
Jameson Weng <weng.jameson126@gmail.com>
2017-05-17 14:34:46 -0400

    lowered learning rate for better results

--- classifier.py
+++ classifier.py
@@ -2,10 +2,13 @@
 
 from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from keras.models import Sequential
+from keras.optimizers import SGD
 from keras.preprocessing import image as kimage
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
+
+import matplotlib.pyplot as plt
 
 import numpy as np
 np.random.seed(123)
@@ -15,12 +18,21 @@ import pandas as pd
 from skimage import io
 from skimage.transform import resize
 
-num_classes = 2
+BATCH_SIZE = 32
+INPUT_WIDTH = 400
+INPUT_HEIGHT = 400
+NUM_CLASSES = 2
+NUM_EPOCHS = 60
 
-def read_and_preprocess_image(file_path):
-    img = kimage.load_img(file_path, target_size=(400,400))
+def preprocess_and_generate_samples(file_path, label):
+    img = kimage.load_img(file_path, target_size=(INPUT_WIDTH, INPUT_HEIGHT))
+    #img.show()
     img_array = kimage.img_to_array(img)
-    return img_array
+
+    #plt.imshow(img_array)
+    #plt.show()
+
+    return [(img_array, label)]
 
 def load_data_set(root_dir):
     data_set = []
@@ -33,50 +45,68 @@ def load_data_set(root_dir):
                 continue
             file_path = file_dir + file_name
             logging.debug('loading image from ' + file_path)
-            data_set.append((read_and_preprocess_image(file_path), label))
+            data_set.extend(preprocess_and_generate_samples(file_path, label))
+            # data_set.append((read_and_preprocess_image(file_path), label))
+            # break
 
     # shuffle the positive & negative examples together
     np.random.shuffle(data_set)
 
     x = np.array([img for img, _ in data_set], dtype='float32')
-    y = np.eye(num_classes, dtype='uint8')[[label for _, label in data_set]]
+    y = np.eye(NUM_CLASSES, dtype='uint8')[[label for _, label in data_set]]
+
     return x, y
+
+def train_cnn_model(x_train, y_train):
+    # define the model
+    logging.info('defining the model')
+
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), activation='relu', 
+        input_shape=x_train.shape[1:], 
+        data_format='channels_last'))
+
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+    # compile model
+    logging.info('compiling the model')
+    sgd = SGD(lr=0.0001)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+    # fit the model
+    # model.fit(x_train, y_train, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE, verbose=1)
+
+    datagen = kimage.ImageDataGenerator(
+            featurewise_center=True,
+            featurewise_std_normalization=True)
+
+    datagen.fit(x_train)
+
+    model.fit_generator(datagen.flow(x_train, y_train, batch_size=BATCH_SIZE),
+            steps_per_epoch=len(x_train) / BATCH_SIZE, epochs=NUM_EPOCHS)
+
+    # save the final weigths
+    model.save('cnn_model.h5')
+
 
 logging.info('loading training set')
 
 x_train, y_train = load_data_set('data/training_set/')
 logging.info('x_train shape: ' + str(x_train.shape))
+logging.info('y_train shape: ' + str(y_train.shape))
 
-#x_test, y_test = load_data_set('data/test_set/')
-
-# define the model
-logging.info('defining the model')
-
-model = Sequential()
-
-model.add(Conv2D(32, (3, 3), activation='relu', 
-    input_shape=x_train.shape[1:], 
-    data_format='channels_last'))
-
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-
-model.add(Dense(num_classes, activation='softmax'))
-
-# compile model
-logging.info('compiling the model')
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-model.fit(x_train, y_train, epochs=40, batch_size=128, verbose=1)
-
-model.save('model.h5')
+train_cnn_model(x_train, y_train)
